3ec68226a9c9a68e958d13f6c31f575b54059f6c
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-03 22:11:42 +0530

    UC2_ADDED

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,6 +3,8 @@ package com.bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Start the Snake and Ladder Game");
-        System.out.println("Single Player at Start Position 0");
+        System.out.println("Player 1 at Start Position 0");
+        int RollDice=(int)(Math.random()*6+1);
+        System.out.println("Player 1 rolls the die and get: "+RollDice);
     }
 }
