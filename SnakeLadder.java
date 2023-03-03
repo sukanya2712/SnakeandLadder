ef24778c1be958c9bb59250f9377c1c73600fb41
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-03 22:28:33 +0530

    UC6_ADDED

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,7 +4,7 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Start the Snake and Ladder Game");
         System.out.println("Single Player at Start Position 0");
-        int positionFirstPlayer = 0;
+        int positionFirstPlayer = 0,count=0;;
         boolean currentTurn = true;
         while(positionFirstPlayer != 100){
             if(currentTurn == true){
@@ -27,8 +27,8 @@ public class SnakeLadder {
                         System.out.println("SNAKE: "+(-RollDice+positionFirstPlayer));
                         System.out.println("Player moves behind by: "+RollDice);
                         positionFirstPlayer = positionFirstPlayer - RollDice;
-                        break;
                 }
+                count++;
                 if(positionFirstPlayer < 0){
                     positionFirstPlayer = 0;
                 }
@@ -38,6 +38,7 @@ public class SnakeLadder {
                 System.out.println();
             }
         }
-        System.out.println("Player 1 reached the winnning Position 100");
+        System.out.println("First Player Rolled Dice "+count+" times");
+        System.out.println("First Player reached the winnning Position 100");
     }
 }
