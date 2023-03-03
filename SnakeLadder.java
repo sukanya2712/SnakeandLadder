a51a0efb60a5c09267013267bbc0b3e7d5f0cb49
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-03 22:32:42 +0530

    UC7_ADDED

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,10 +3,9 @@ package com.bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Start the Snake and Ladder Game");
-        System.out.println("Single Player at Start Position 0");
-        int positionFirstPlayer = 0,count=0;;
+        int positionFirstPlayer = 0, positionSecondPlayer = 0,count1=0,count2=0;
         boolean currentTurn = true;
-        while(positionFirstPlayer != 100){
+        while(positionFirstPlayer != 100 && positionSecondPlayer != 100){
             if(currentTurn == true){
                 System.out.println("Player 1 currently at Position "+ positionFirstPlayer);
                 int RollDice=(int)(Math.random()*6+1);
@@ -17,6 +16,7 @@ public class SnakeLadder {
                     case 1:
                         System.out.println("NO PLAY: "+positionFirstPlayer);
                         System.out.println("Player stays in the same position");
+                        currentTurn = false;
                         break;
                     case 2:
                         System.out.println("LADDER: "+(RollDice+positionFirstPlayer));
@@ -27,18 +27,59 @@ public class SnakeLadder {
                         System.out.println("SNAKE: "+(-RollDice+positionFirstPlayer));
                         System.out.println("Player moves behind by: "+RollDice);
                         positionFirstPlayer = positionFirstPlayer - RollDice;
+                        currentTurn = false;
                 }
-                count++;
+                count1++;
                 if(positionFirstPlayer < 0){
                     positionFirstPlayer = 0;
                 }
                 if(positionFirstPlayer > 100){
                     positionFirstPlayer -= RollDice;
                 }
-                System.out.println();
+
             }
+            else {
+                System.out.println("Player 2 currently at Position "+ positionSecondPlayer);
+                int RollDice=(int)(Math.random()*6+1);
+                System.out.println("dice rolls by: "+RollDice);
+                int currentOption = (int)(Math.random()*3+1);
+                switch(currentOption)
+                {
+                    case 1:
+                        System.out.println("NO PLAY: "+positionSecondPlayer);
+                        System.out.println("Player stays in the same position");
+                        currentTurn = true;
+                        break;
+                    case 2:
+                        System.out.println("LADDER: "+(RollDice+positionSecondPlayer));
+                        System.out.println("Player moves ahead by: "+RollDice);
+                        positionSecondPlayer = positionSecondPlayer + RollDice;
+                        break;
+                    case 3:
+                        System.out.println("SNAKE: "+(-RollDice+positionSecondPlayer));
+                        System.out.println("Player moves behind by: "+RollDice);
+                        positionSecondPlayer = positionSecondPlayer - RollDice;
+                        currentTurn = true;
+                }
+                count2++;
+                if(positionSecondPlayer < 0){
+                    positionSecondPlayer = 0;
+                }
+                if(positionSecondPlayer > 100){
+                    positionSecondPlayer -= RollDice;
+                }
+
+            }
+            System.out.println(" ");
         }
-        System.out.println("First Player Rolled Dice "+count+" times");
-        System.out.println("First Player reached the winnning Position 100");
+
+        if(positionFirstPlayer == 100){
+            System.out.println("First Player Rolled Dice "+count1+" times");
+            System.out.println("First Player won");
+        }
+        else {
+            System.out.println("Second Player Rolled Dice "+count2+" times");
+            System.out.println("second Player won");
+        }
     }
 }
